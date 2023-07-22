c7051e60efa0c7e38a244fe5f91adc5b6231003c
KatouMegumii <yi.ding-14@postgrad.manchester.ac.uk>
2023-07-22 19:59:57 +0100

    code simplified

--- MainWindow.py
+++ MainWindow.py
@@ -1,13 +1,10 @@
-import numpy as np
+import cv2
 from PySide6.QtCore import Qt
-from PySide6.QtGui import QIcon, QPainter, QPixmap
-from PySide6.QtWidgets import QApplication, QMainWindow, QGraphicsView, QGraphicsScene, QFileDialog
-from matplotlib import pyplot as plt
-
-from main_structure import Ui_Form
-from ImageViewer import ImageViewer
+from PySide6.QtGui import QIcon, QImage, QPixmap
+from PySide6.QtWidgets import QApplication, QMainWindow, QFileDialog, QGraphicsScene, QGraphicsView
 from segment_anything import sam_model_registry, SamPredictor
 
+from main_structure import Ui_Form
 
 sam_checkpoint = "D:/Python Coding/segment-model/sam_vit_h_4b8939.pth"
 model_type = "vit_h"
@@ -19,22 +16,12 @@ sam.to(device=device)
 predictor = SamPredictor(sam)
 
 
-def button_enter(button, icon_path):
-    button.setIcon(QIcon(icon_path))
-
-
-def button_leave(button, icon_path, icon_path_checked):
-    if button.isChecked():
-        button.setIcon(QIcon(icon_path_checked))
-    else:
-        button.setIcon(QIcon(icon_path))
-
-
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.scene_pos = None
+        self.offset = None
+        self.draggable = None
 
         self.setWindowFlag(Qt.WindowType.FramelessWindowHint)
         self.setAttribute(Qt.WA_TranslucentBackground)
@@ -43,17 +30,12 @@ class MainWindow(QMainWindow):
         self.ui.setupUi(self)
 
         self.image_viewer = ImageViewer()
-        self.image_viewer.widget()
         self.ui.image_view.setScene(self.image_viewer.scene)
 
         self.ui.button_import.clicked.connect(self.image_viewer.import_image)
 
         self.ui.button_close.clicked.connect(self.close)
         self.ui.button_minimize.clicked.connect(self.showMinimized)
-
-        self.ui.button_point_mode.leaveEvent = lambda event: button_leave(self.ui.button_point_mode, '../easy-to-segment/UI icon/point.svg', '../easy-to-segment/UI icon/point-white.svg')
-        self.ui.button_box_mode.leaveEvent = lambda event: button_leave(self.ui.button_box_mode, '../easy-to-segment/UI icon/box.svg', '../easy-to-segment/UI icon/box-white.svg')
-        self.ui.button_view_mode.leaveEvent = lambda event: button_leave(self.ui.button_view_mode, '../easy-to-segment/UI icon/view.svg', '../easy-to-segment/UI icon/view-white.svg')
 
         self.ui.button_view_mode.setIcon(QIcon('../easy-to-segment/UI icon/view-white.svg'))
 
@@ -65,10 +47,12 @@ class MainWindow(QMainWindow):
             button.setIcon(QIcon('../easy-to-segment/UI icon/point-white.svg'))
             self.ui.button_view_mode.setIcon(QIcon('../easy-to-segment/UI icon/view.svg'))
             self.ui.button_box_mode.setIcon(QIcon('../easy-to-segment/UI icon/box.svg'))
+
         elif button == self.ui.button_box_mode:
             button.setIcon(QIcon('../easy-to-segment/UI icon/box-white.svg'))
             self.ui.button_point_mode.setIcon(QIcon('../easy-to-segment/UI icon/point.svg'))
             self.ui.button_view_mode.setIcon(QIcon('../easy-to-segment/UI icon/view.svg'))
+
         elif button == self.ui.button_view_mode:
             button.setIcon(QIcon('../easy-to-segment/UI icon/view-white.svg'))
             self.ui.button_point_mode.setIcon(QIcon('../easy-to-segment/UI icon/point.svg'))
@@ -77,8 +61,10 @@ class MainWindow(QMainWindow):
     def group_button_press(self, button):
         if button == self.ui.button_point_mode:
             button.setIcon(QIcon('../easy-to-segment/UI icon/point-white.svg'))
+
         elif button == self.ui.button_box_mode:
             button.setIcon(QIcon('../easy-to-segment/UI icon/box-white.svg'))
+
         elif button == self.ui.button_view_mode:
             button.setIcon(QIcon('../easy-to-segment/UI icon/view-white.svg'))
 
@@ -86,9 +72,6 @@ class MainWindow(QMainWindow):
         if event.button() == Qt.LeftButton:
             self.draggable = True
             self.offset = event.pos()
-            view_pos = event.pos()
-            scene_pos = self.ui.image_view.mapToScene(view_pos)
-            self.scene_pos = np.array([scene_pos.x(), scene_pos.y()])
 
     def mouseMoveEvent(self, event):
         if self.draggable and event.buttons() == Qt.LeftButton:
@@ -100,7 +83,29 @@ class MainWindow(QMainWindow):
             self.draggable = False
 
 
-test = MainWindow()
+class ImageViewer(QGraphicsView):
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+
+        self.pixmap_item = None
+        self.pixmap = None
+        self.scene = QGraphicsScene()
+
+    def import_image(self):
+        file_dialog = QFileDialog(self)
+        file_dialog.setWindowTitle("Select Image File")
+        file_dialog.setNameFilter("Image Files (*.png *.jpg *.bmp)")
+        file_dialog.setFileMode(QFileDialog.ExistingFile)
+        if file_dialog.exec_():
+            image_path = file_dialog.selectedFiles()[0]
+
+        image = cv2.imread(image_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        predictor.set_image(image)
+        q_image = QImage(image.data, image.shape[1], image.shape[0], QImage.Format_RGB888)
+        self.pixmap = QPixmap.fromImage(q_image)
+        self.pixmap_item = self.scene.addPixmap(self.pixmap)
+        self.scene.addItem(self.pixmap_item)
 
 
 if __name__ == '__main__':
